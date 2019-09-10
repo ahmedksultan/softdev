c228736e62ae973a80140a68f6a5dcffba6b441c
asultan00 <asultan00@stuy.edu>
2019-09-10 15:10:01 -0400

    edited studentPicker to pick completely random

--- studentPicker.py
+++ studentPicker.py
@@ -1,27 +1,55 @@
 import random
 
+#   ATTEMPT I — choosing random character from defined 'krew'
+
 KREWES = {
-    'one': ['ahmed', 'joseph', 'max', 'alma', 'julian', 'mykolyk'],
-    'two': ['tejas', 'leon', 'jason', 'tommy', 'manfred', 'fahim'],
-    'three': ['bruh', 'moment', 'qbert', 'john', 'nora', 'lydia']
+    'alpha': ['ness', 'mario', 'luigi', 'mewtwo', 'banjo', 'roy', 'falco'],
+    'beta': ['donkey kong', 'pikachu', 'yoshi', 'captain falcon', 'lucas', 'link', 'kirby'],
+    'gamma': ['sonic', 'cloud', 'king dedede', 'snake', 'jigglypuff', 'fox', 'zelda']
 }
 
-#function
-def studentPicker(name):
-    x = random.randrange(0, (len(KREWES[name])))
-    print(KREWES[name][x])
+#   function to pick random character from dictionary of lists, given specific list
+def studentPicker(krew):
+    x = random.randrange(0, (len(KREWES[krew])))
+    print(KREWES[krew][x])
 
 
-studentPicker('one')
-studentPicker('one')
-studentPicker('one')
-studentPicker('one')
-studentPicker('two')
-studentPicker('two')
-studentPicker('two')
-studentPicker('two')
-studentPicker('three')
-studentPicker('three')
-studentPicker('three')
-studentPicker('three')
-studentPicker('three')
+print("--- semi random ---")
+studentPicker('alpha')
+studentPicker('alpha')
+studentPicker('alpha')
+studentPicker('alpha')
+studentPicker('alpha')
+studentPicker('beta')
+studentPicker('beta')
+studentPicker('beta')
+studentPicker('beta')
+studentPicker('beta')
+studentPicker('gamma')
+studentPicker('gamma')
+studentPicker('gamma')
+studentPicker('gamma')
+studentPicker('gamma')
+
+#   ATTEMPT II — choosing random character from random 'krew'
+
+def studentPicker2():
+    x = random.randrange(0, (len(KREWES)))
+    if (x == 0):
+        name = 'alpha'
+    elif (x == 1):
+        name = 'beta'
+    else:
+        name = 'gamma'
+    y = random.randrange(0, (len(KREWES[name])))
+    print(KREWES[name][y])
+
+print("--- completely random ---")
+studentPicker2()
+studentPicker2()
+studentPicker2()
+studentPicker2()
+studentPicker2()
+studentPicker2()
+    
+
