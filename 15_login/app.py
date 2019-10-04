b7626006288970d100b24cff252346d7b5ae3819
asultan00 <asultan00@stuy.edu>
2019-10-04 10:50:07 -0400

    completed & fixed assignment

--- 15_login/app.py
+++ 15_login/app.py
@@ -8,12 +8,27 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
+from flask import session
+import os
 
 app = Flask(__name__)
 
+# hard-coded login credentials
+user = "dirtydashers"
+pswd = "password99"
+
+# creating secret key for session (staying logged in)
+app.secret_key = os.urandom(32)
+
+# preparing a string variable for outputting error messages
+errorMsg = ""
+
 @app.route('/')
 def root():
-    return render_template("login.html")
+    # checking to see if the user has already logged in
+    if 'user' in session:
+        return redirect(url_for('welcome'))
+    return redirect(url_for('login'))
 
 @app.route('/welcome')
 def welcome():
@@ -23,12 +38,32 @@ def welcome():
 def error():
     return render_template("error.html")
 
+@app.route('/login')
+def login():
+    return render_template(
+        "login.html",
+        error = errorMsg)
+
 @app.route("/auth")
 def auth():
-    if request.args["username"] == "qbert" and request.args["password"] == "ahmednahi":
-        return redirect(url_for('welcome'))
+    inputUser = request.args['username']
+    inputPswd = request.args['password']
+    # checking to see if input credentials match hard coded credentials
+    if inputUser == user:
+        if inputPswd == pswd:
+            session['user'] = inputUser
+            return redirect(url_for('welcome'))
+        else:
+            errorMsg = "[INVALID PASSWORD]"
+            return redirect(url_for('error'))
     else:
+        errorMsg = "[INVALID USERNAME and/or PASSWORD]"
         return redirect(url_for('error'))
+
+@app.route("/logout")
+def logout():
+    session.pop('user')
+    return redirect(url_for('root'))
 
 if __name__ == "__main__":
     app.debug = True
