bfac03edc5f8a3c66bb74ae7b4b2a378a4c44e94
asultan00 <asultan00@stuy.edu>
2019-10-08 14:46:20 -0400

    completed assignment

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -1,26 +1,43 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev
-#skeleton :: SQLITE3 BASICS
-#Oct 2019
+# Team SulTan - Ahmed Sultan and Manfred Tan
+# SoftDev1 pd9
+# K17 -- No Trouble
+# 2019-10-08
 
 import sqlite3  # enable control of an sqlite database
 import csv  # facilitate CSV I/O
 
-
-DB_FILE = "discobandit.db"
-
-db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
+# creating/accessing SQLite3 database file
+DB_FILE = "school.db"
+db = sqlite3.connect(DB_FILE) 
 c = db.cursor()  # facilitate db ops
 
-#==========================================================
+# creating students table
+command = "CREATE TABLE students(ID INT PRIMARY KEY, NAME TEXT NO NULL, AGE INT NO NULL);"
+c.execute(command)
 
-# < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+# reading through students.csv, populating students table
+# information ::: id, name, age
+with open('students.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        command = "INSERT INTO students VALUES(" + row['id'] + ", '" + row['name'] + "', " + row['age'] + ");"
+        c.execute(command)
 
+###########################################################
 
-command = ""          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
+# creating courses table
+command = "CREATE TABLE courses(ID INT, CODE TEXT NO NULL, MARK INT NO NULL);"
+c.execute(command)
 
-#==========================================================
+# reading through course.csv, populating courses table
+# information ::: id, code, mark
 
-db.commit()  # save changes
-db.close()  # close database
+with open('courses.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        command = "INSERT INTO courses VALUES(" + row['id'] + ", '" + row['code'] + "', " + row['mark'] + ");"
+        c.execute(command)
+
+# saving and exiting
+db.commit()
+db.close()
