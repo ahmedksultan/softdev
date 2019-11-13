6452ff2c486226bdf937ff7bdb83fb22acdf1b02
asultan00 <asultan00@stuy.edu>
2019-11-13 11:25:56 -0500

    trying to figure out what is wrong with 24_rest

---
+++ 24_rest/app.py
@@ -0,0 +1,21 @@
+# Ahmed Sultan
+# SoftDev 1 pd 9
+# K24 -- A RESTful Journey Skyward
+# 2019-11-12
+
+from flask import Flask, render_template
+import urllib
+import json
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    u = urllib.request.urlopen(
+        "https://api.nasa.gov/planetary/apod?api_key=I9chRrUh43ayV1eRZa53qAmYznCGebkcoAtye5kP")
+    response = u.read()
+    data = json.loads(response)
+    return render_template("index.html", header = data['title'], picture = data['url'], info = data['explanation'])
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
