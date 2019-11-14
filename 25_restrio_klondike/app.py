52653d29ad8be124a9f1c9297a1b9b40506ed94b
asultan00 <asultan00@stuy.edu>
2019-11-13 19:41:19 -0500

    added countries API

--- 25_restrio_klondike/app.py
+++ 25_restrio_klondike/app.py
@@ -5,6 +5,7 @@
 
 from flask import Flask, render_template
 import urllib
+import random
 import json
 
 app = Flask(__name__)
@@ -13,16 +14,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
+    # accessing countries API
     u = urllib.request.urlopen("https://restcountries.eu/rest/v2")
     response = u.read()
     data = json.loads(response)
 
-    u1 = urllib.request.urlopen("https://api.exchangerate-api.com/v4/latest/CAD")
-    response1 = u1.read()
-    data1 = json.loads(response)
+    # picking random country from API response
+    rand_num = random.randint(0, 249)
+    ctry_name = data[rand_num]['name']
+    ctry_capital = data[rand_num]['capital']
+    ctry_flag = data[rand_num]['flag']
+    ctry_currency = data[rand_num]['currencies'][0]['name']
+    ctry_languages = []
+    for i in data[rand_num]['languages']:
+        ctry_languages.append(i['name'])
 
-    return render_template("index.html", header=data[95]['name'], info=data[95]['capital'], pic=data[95]['flag'], currency=data[95]['currencies'][0]['name'], info2=data1['rates']['CNY'])
-
+    return render_template("index.html", name=ctry_name, flag=ctry_flag, capital=ctry_capital, currency=ctry_currency, languages=ctry_languages)
 
 if __name__ == "__main__":
     app.debug = True
