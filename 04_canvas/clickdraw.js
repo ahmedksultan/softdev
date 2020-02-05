c5aa399bfb500f6ea2a7fa994c6d6a2da1c0dd82
asultan00 <asultan00@stuy.edu>
2020-02-05 15:20:15 -0500

    finished draw box?

--- 04_canvas/clickdraw.js
+++ 04_canvas/clickdraw.js
@@ -1,20 +1,26 @@
+// state variable: either dot or box
 var mode = "dot"
 
+// canvas
 var c = document.getElementById("slate")
 var ctx = c.getContext("2d")
 
+// clear canvas
 function clear() {
      ctx.clearRect(0, 0, 300, 300);
 }
 
+// sets mode to dot
 function dotmode() {
      mode = "dot"
 }
 
-function rectmode() {
-     mode = "rect"
+// sets mode to box
+function boxmode() {
+     mode = "box"
 }
 
+// drawing function, takes in mode and cursor location
 function draw(e) {
      console.log(e)
      if(mode==="dot"){
@@ -24,6 +30,13 @@ function draw(e) {
           ctx.fill(circle);
           console.log("dot")
      }
+     else {
+          var box = new Path2D();
+          box.moveTo(e.offsetX, e.offsetY);
+          box.rect(e.offsetX, e.offsetY, 20, 20);
+          ctx.fill(box);
+          console.log("box")
+     }
 }
 
 var clearbutton = document.getElementById("clear")
@@ -32,7 +45,7 @@ clearbutton.addEventListener('click',clear);
 var dotbutton = document.getElementById("dot")
 dotbutton.addEventListener('click', dotmode);
 
-var rectbutton = document.getElementById("rect")
-rectbutton.addEventListener('click', rectmode);
+var boxbutton = document.getElementById("box")
+boxbutton.addEventListener('click', boxmode);
 
 c.addEventListener('click', draw)
