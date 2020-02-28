9079f05d970730a30163b71300b2210006470721
asultan00 <asultan00@stuy.edu>
2020-02-28 00:13:19 -0500

    added a few comments

--- 09_mongo/yummymongopy.py
+++ 09_mongo/yummymongopy.py
@@ -19,26 +19,30 @@ restaurants = db.restaurants
 
 result = restaurants.insert_many(items)
 
+# find restaurants by BOROUGH
 def findBorough(borough):
     for restaurant in db.restaurants.find({"borough": borough}):
         pprint.pprint(restaurant)
 
+# find restaurants by ZIPCODE
 def findZipcode(zipcode):
     for restaurant in db.restaurants.find({"address.zipcode": zipcode}):
         pprint.pprint(restaurant)
 
+# find restaurants by ZIPCODE and =GRADE 
 def findZipcodeGrade(zipcode, grade):
      for restaurant in db.restaurants.find({'address.zipcode': zipcode, 'grades': {'$elemMatch': {'grade': grade}}}):
           pprint.pprint(restaurant)
 
+# find restaurants by ZIPCODE and <SCORE
 def findZipcodeScoreBelow(zipcode, score):
      for restaurant in db.restaurants.find({'address.zipcode': zipcode, 'grades': {'$elemMatch': {'score': {'$lt': score}}}}):
           pprint.pprint(restaurant)
 
+# ~creative~: find restaurants by BOROUGH, CUISINE and >SCORE
 def findBoroughCuisineScore(borough, cuisine, score):
      for restaurant in db.restaurants.find({'borough': borough, 'cuisine': cuisine, 'grades': {'$elemMatch': {'score': {'$gte': score}}}}):
           pprint.pprint(restaurant)
 
-findBoroughCuisineScore('Manhattan', 'Indian', 10)
-
+# clear
 restaurants.delete_many({})
